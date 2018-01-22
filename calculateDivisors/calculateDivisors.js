41312ee132fdb0ed6716e29b618d56f6e939347b
jaderyan <jadeelizabethryan@outlook.com>
2018-01-22 12:21:13 +0000

    calculateDivisors kata

---
+++ calculateDivisors/calculateDivisors.js
@@ -0,0 +1,7 @@
+var calculateDivisors = function (number) {
+    // Your code here
+  };
+  
+  if (typeof module !== 'undefined') {
+    module.exports = {calculateDivisors};
+  }
---
+++ calculateDivisors/spec/calculateDivisors.spec.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const {expect} = require('chai');
+const {calculateDivisors} = require(path.join(__dirname, '..', './calculateDivisors.js'));
+
+describe('calculateDivisors()', function () {
+  it('ADD FIRST TEST AND RENAME', function () {
+    
+  });
+});
