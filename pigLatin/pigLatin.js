09129382e33adb6fcebcbc13895f67b43f259595
jaderyan <jadeelizabethryan@outlook.com>
2018-01-22 12:25:27 +0000

    pigLatin kata

---
+++ pigLatin/pigLatin.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var pigLatin = function (str) {
+    // Your code here
+  };
+  
+  if (typeof module !== 'undefined') {
+    module.exports = {pigLatin};
+  }
---
+++ pigLatin/spec/pigLatin.spec.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {pigLatin} = require(path.join(__dirname, '..', './pigLatin.js'));
+
+describe('pigLatin()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function () {
+    
+  });
+});
