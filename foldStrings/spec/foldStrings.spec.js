94bcc0b6d50df1e64a438b9dfbe801d453c62fca
Christopher Peers <christopher_peers@outlook.com>
2018-01-29 11:39:58 +0000

    initial commit

---
+++ foldStrings/spec/foldStrings.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var {foldString} = require('../foldStrings');
+
+describe('foldString()', function () {
+  it('ADD TEST AND RENAME', function() {
+
+  });
+
+});
