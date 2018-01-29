7ef4332a95cfec060fa8d94f0a951a87d988bc3d
Christopher Peers <christopher_peers@outlook.com>
2018-01-29 11:42:16 +0000

    initial commit

---
+++ dnaPairs/spec/dnaPairs.spec.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {dnaPairs} = require(path.join(__dirname, '..', './dnaPairs.js'));
+
+describe('dnaPairs()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function() {
+
+  });
+
+});
