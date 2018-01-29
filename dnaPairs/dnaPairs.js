a801b79631f5b662cf32d7948907cbac224c19b6
Christopher Peers <christopher_peers@outlook.com>
2018-01-29 11:42:56 +0000

    initial commit

---
+++ dnaPairs/dnaPairs.js
@@ -0,0 +1,10 @@
+// Please do not change the name of this function
+var dnaPairs = function (dna) {
+  
+	// your coder here
+
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {dnaPairs};
+}
