f089271ca39a7383d24f96490b3b6faee8626295
James Steele <jamessteele87@outlook.com>
2018-02-01 19:20:16 +0000

    test for calculatedivisors

--- calculateDivisors/calculateDivisors.js
+++ calculateDivisors/calculateDivisors.js
@@ -1,16 +1,14 @@
-var calculateDivisors = function (num) {
-
-  let divisibleNumbers = 0;
-
-  for (var i = 1; i < num; i++) {
-    if (i % 3 === 0) {
-      divisibleNumbers += i;
-    
-    }  
+var calculateDivisors = function (number) {
+  if (number < 3) return 0;
+  
+  let divisorsOfNumber = 0;
+  
+  for (let i = 0; i < number; i ++) {
+    if (i % 3 === 0 || i % 5 === 0) divisorsOfNumber += i;
   }
+  return divisorsOfNumber;
 
- return divisibleNumbers;
-
+  
   };
   
   if (typeof module !== 'undefined') {
